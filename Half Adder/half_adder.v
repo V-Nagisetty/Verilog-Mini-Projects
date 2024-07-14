35c68e54237f0c71aedae15f4a3ad8b66f21d37f
V-Nagisetty <138035027+V-Nagisetty@users.noreply.github.com>
2024-07-14 22:38:32 +0530

    Delete Half Adder/half_adder.v

--- Half Adder/half_adder.v
+++
@@ -1,9 +0,0 @@
-module half_adder(a,b,sum,carry);              
-
-input  a,b;                          
-output sum,carry;
-
-assign sum   =  a ^ b ;//xor operation
-assign carry =  a & b ;//and operation
-  
-endmodule 
